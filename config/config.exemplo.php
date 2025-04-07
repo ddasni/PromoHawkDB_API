549b70f9b8a5a2cf2472f8e996104c57b6adb181
ddasni <danielsousamelo5@gmail.com>
2025-04-07 20:53:32 -0300

    arquivos de conexão

---
+++ config/config.exemplo.php
@@ -0,0 +1,4 @@
+define('DB_HOST', ''); // localhost ou url do server
+define('DB_NAME', ''); // Nome do Banco de dados
+define('DB_USER', ''); // Usuario
+define('DB_PASS', ''); // Senha
---
+++ database/conexao.php
@@ -0,0 +1,22 @@
+<?php
+require_once __DIR__ . '/../config/config.php';
+
+class Conexao{  
+    // Atributo estático para armazenar a única instância da conexão
+    private static $instancia;
+
+    // Método estático para obter a conexão com o banco de dados
+    public static function getConexao(){
+        // Verifica se a instância já existe
+        if(!isset(self::$instancia)){
+            // Cria uma nova conexão com o banco de dados
+            self::$instancia = new PDO('"mysql:host=". DB_HOST .";dbname=". DB_NAME ."; charset=utf8", DB_USER, DB_PASS');
+            return self::$instancia;
+        }
+        else{
+            // Retorna a instância da conexão
+            return self::$instancia;
+        }  
+    }
+  }
+?>
