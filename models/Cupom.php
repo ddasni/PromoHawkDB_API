9bbfe53842d0bd18bdf605f66389988af5cf4d3d
ddasni <danielsousamelo5@gmail.com>
2025-04-10 16:50:10 -0300

    Alterando os nome e removendo alguns metodos e atributos

--- models/Cupom.php
+++ models/Cupom.php
@@ -1,52 +1,36 @@
 <?php
-// Classe entidade Usuario
-// Representa uma Usuario com seus atributos
+// Classe entidade Cupom
+// Representa uma Cupom com seus atributos
 
-  class Usuario{
+  class Cupom{
     // Atributos privados da classe Pessoa
-    private $cpf, $nome, $profissao, $telefone, $email;
+    private $codigo, $desconto, $validade;
 
-    // Método para definir o CPF da pessoa
-    public function setCpf($cpf){
-        $this->cpf = $cpf;
+    // Método para definir o codigo do Cupom
+    public function setCodigo($codigo){
+        $this->codigo = $codigo;
     }
 
-    // Método para obter o CPF da pessoa
-    public function getCpf(){
-        return $this->cpf;
+    // Método para obter o codigo do Cupom
+    public function getCodigo(){
+        return $this->codigo;
     }
 
     // os outros métodos getters e setters para os demais atributos
-    public function setNome($nome){
-        $this->nome = $nome;
+    public function setDesconto($desconto){
+        $this->desconto = $desconto;
     }
 
-    public function getNome(){
-        return $this->nome;
+    public function getDesconto(){
+        return $this->desconto;
     }
 
-    public function setProfissao($profissao){
-        $this->profissao = $profissao;
+    public function setValidade($validade){
+        $this->validade = $validade;
     }
 
-    public function getProfissao(){
-        return $this->profissao;
-    }
-
-    public function setTelefone($telefone){
-        $this->telefone = $telefone;
-    }
-
-    public function getTelefone(){
-        return $this->telefone;
-    }
-
-    public function setEmail($email){
-        $this->email = $email;
-    }
-
-    public function getEmail(){
-        return $this->email;
+    public function getValidade(){
+        return $this->validade;
     }
   }
 ?>
