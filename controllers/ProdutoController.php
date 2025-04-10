8ac187def4cba92e8f45431be5bb88336f4074a2
ddasni <danielsousamelo5@gmail.com>
2025-04-10 16:41:34 -0300

    Adicionando os controllers

---
+++ controllers/ProdutoController.php
@@ -0,0 +1,45 @@
+<?php
+require_once './models/Usuario.php';
+require_once './dao/UsuarioDAO.php';
+
+class UsuarioController {
+    public function consultar() {
+        $usuarioDAO = new UsuarioDAO();
+        echo json_encode($usuarioDAO->consultar());
+    }
+
+    public function cadastrar() {
+        $usuarioDAO = new UsuarioDAO();
+        $usuario = new Usuario();
+
+        $usuario->setCpf(filter_input(INPUT_POST, 'cpf'));
+        $usuario->setNome(filter_input(INPUT_POST, 'nome'));
+        $usuario->setProfissao(filter_input(INPUT_POST, 'profissao'));
+        $usuario->setTelefone(filter_input(INPUT_POST, 'telefone'));
+        $usuario->setEmail(filter_input(INPUT_POST, 'email'));
+
+        echo json_encode($usuarioDAO->cadastrar($usuario));
+    }
+
+    public function atualizar() {
+        $usuarioDAO = new UsuarioDAO();
+        $usuario = new Usuario();
+
+        $usuario->setCpf(filter_input(INPUT_POST, 'cpf'));
+        $usuario->setNome(filter_input(INPUT_POST, 'nome'));
+        $usuario->setProfissao(filter_input(INPUT_POST, 'profissao'));
+        $usuario->setTelefone(filter_input(INPUT_POST, 'telefone'));
+        $usuario->setEmail(filter_input(INPUT_POST, 'email'));
+
+        echo json_encode($usuarioDAO->atualizar($usuario));
+    }
+
+    public function deletar() {
+        $usuarioDAO = new UsuarioDAO();
+        $usuario = new Usuario();
+
+        $usuario->setCpf(filter_input(INPUT_POST, 'cpf'));
+
+        echo json_encode($usuarioDAO->deletar($usuario));
+    }
+}
--- controllers/UsuarioController.php
+++ controllers/UsuarioController.php
@@ -1,49 +1,45 @@
 <?php
-    // Configura os cabeçalhos para permitir requisições de diferentes origens (CORS)
-    //CORS: Permite que requisições originadas de outros domínios acessem a API
-    header('Access-Control-Allow-Origin: *');
-    //Methods HTTP: Especifica os métodos HTTP permitidos (GET, POST, OPTIONS)
-    header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
-    //Headers: Define os cabeçalhos permitidos nas requisições (Content-Type)
-    header('Access-Control-Allow-Headers: Content-Type');
-    //Content-Type: Define o tipo de conteúdo da resposta como JSON
-    header("Content-Type: application/json; charset=UTF-8");
-   
-    // Inclui os arquivos com as classes Pessoa e PessoaDAO
-    include "Usuario.php";
-    include "UsuarioDAO.php";
-    
-    // Cria instâncias das classes
-    $usuario = new Usuario();
-    $usuarioDAO = new UsuarioDAO();
-    
-    // Verifica se a ação é consultar todas as pessoas
-    if(isset($_GET['getPessoa'])){
-         // Consulta todas as pessoas no banco de dados e retorna em formato JSON
+require_once './models/Usuario.php';
+require_once './dao/UsuarioDAO.php';
+
+class UsuarioController {
+    public function consultar() {
+        $usuarioDAO = new UsuarioDAO();
         echo json_encode($usuarioDAO->consultar());
     }
-    else if(isset($_GET['cadPessoa'])){
-        // Extrai os dados da pessoa da requisição e cria um objeto Pessoa
-        $usuario->setCpf(filter_input(INPUT_POST ,'cpf'));
-        $usuario->setNome(filter_input(INPUT_POST ,'nome'));
-        $usuario->setProfissao(filter_input(INPUT_POST ,'profissao'));
-        $usuario->setTelefone(filter_input(INPUT_POST ,'telefone'));
-        $usuario->setEmail(filter_input(INPUT_POST ,'email'));
-        // Insere a pessoa no banco de dados e retorna o resultado em JSON
+
+    public function cadastrar() {
+        $usuarioDAO = new UsuarioDAO();
+        $usuario = new Usuario();
+
+        $usuario->setCpf(filter_input(INPUT_POST, 'cpf'));
+        $usuario->setNome(filter_input(INPUT_POST, 'nome'));
+        $usuario->setProfissao(filter_input(INPUT_POST, 'profissao'));
+        $usuario->setTelefone(filter_input(INPUT_POST, 'telefone'));
+        $usuario->setEmail(filter_input(INPUT_POST, 'email'));
+
         echo json_encode($usuarioDAO->cadastrar($usuario));
     }
-    else if(isset($_GET['delPessoa'])){
-        // utiliza a mesma lógica anterior, agora para excluir uma pessoa
-        $usuario->setCpf(filter_input(INPUT_POST ,'cpf'));
-        echo json_encode($usuarioDAO->deletar($usuario));
-    }
-    else if(isset($_GET['atuPessoa'])){
-        // utiliza a mesma lógica anterior, agora para atualizar uma pessoa
-        $usuario->setCpf(filter_input(INPUT_POST ,'cpf'));
-        $usuario->setNome(filter_input(INPUT_POST ,'nome'));
-        $usuario->setProfissao(filter_input(INPUT_POST ,'profissao'));
-        $usuario->setTelefone(filter_input(INPUT_POST ,'telefone'));
-        $usuario->setEmail(filter_input(INPUT_POST ,'email'));
+
+    public function atualizar() {
+        $usuarioDAO = new UsuarioDAO();
+        $usuario = new Usuario();
+
+        $usuario->setCpf(filter_input(INPUT_POST, 'cpf'));
+        $usuario->setNome(filter_input(INPUT_POST, 'nome'));
+        $usuario->setProfissao(filter_input(INPUT_POST, 'profissao'));
+        $usuario->setTelefone(filter_input(INPUT_POST, 'telefone'));
+        $usuario->setEmail(filter_input(INPUT_POST, 'email'));
+
         echo json_encode($usuarioDAO->atualizar($usuario));
     }
-?> 
+
+    public function deletar() {
+        $usuarioDAO = new UsuarioDAO();
+        $usuario = new Usuario();
+
+        $usuario->setCpf(filter_input(INPUT_POST, 'cpf'));
+
+        echo json_encode($usuarioDAO->deletar($usuario));
+    }
+}
