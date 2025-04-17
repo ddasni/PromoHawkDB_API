90346e2627b38ba36a00d6b6540201f5de10414b
ddasni <danielsousamelo5@gmail.com>
2025-04-17 20:16:56 -0300

    Finalizando o DAO do produto

--- dao/ProdutoDAO.php
+++ dao/ProdutoDAO.php
@@ -3,104 +3,14 @@
 // Inclui o arquivo de conexão com o banco de dados
 include "conexao.php";
 
-    // Classe responsável por realizar operações de acesso a dados (DAO) para a entidade Pessoa
+    // Classe responsável por realizar operações de acesso a dados (DAO) para a entidade Cupom
     class ProdutoDAO{
-
-        // Método para cadastrar uma nova pessoa no banco de dados
-        // Insere dados na tabela 'pessoa' e 'contato'
-        public function cadastrar(Usuario $u){
-
-            // SQL para inserir dados na tabela 'pessoa'
-            $sql_usuario = "insert into pessoa (cpf_pessoa, nome_pessoa, profissao_pessoa) 
-            values (?, ?, ?)";
-            // SQL para inserir dados na tabela 'contato'
-            $sql_contato = "insert into contato(telefone_contato, email_contato, pessoa_contato) values
-            (?, ?, ?)";
-
-            //instanciar o objeto de conexão
-            $bd = new Conexao();
-            $con = $bd->getConexao();
-
-            // Prepara e executa as consultas SQL
-            $valor_usuario = $con->prepare($sql_usuario);
-            $valor_usuario->bindValue(1, $u->getCpf());
-            $valor_usuario->bindValue(2, $u->getNome());
-            $valor_usuario->bindValue(3, $u->getProfissao());
-
-            // Prepara e executa as consultas SQL
-            $valor_contato = $con->prepare($sql_contato);
-            $valor_contato->bindValue(1, $u->getTelefone());
-            $valor_contato->bindValue(2, $u->getEmail());
-            $valor_contato->bindValue(3, $u->getCpf());        
-
-            $resultado_pessoa = $valor_usuario->execute();
-            $resultado_contato = $valor_contato->execute();
-
-            // Verifica se ambas as inserções foram bem-sucedidas
-            if($resultado_pessoa && $resultado_contato){
-                return "cadastrado com sucesso";
-            }else{
-                return "erro ao cadastrar";
-            }
-        }
-
-        //os demais métodos seguem a mesma lógica do primeiro
-        public function deletar(Usuario $u){
-            $sql_usuario = "delete from pessoa where cpf_pessoa=?";
-            $sql_contato = "delete from contato where pessoa_contato = ?";
-
-            $bd = new Conexao();
-            $con = $bd->getConexao();
-
-            $valor_usuario = $con->prepare($sql_usuario);
-            $valor_usuario->bindValue(1, $u->getCpf());
-
-            $valor_contato = $con->prepare($sql_contato);
-            $valor_contato->bindValue(1, $u->getCpf());        
-            
-            $resultado_pessoa = $valor_usuario->execute();
-            $resultado_contato = $valor_contato->execute();
-
-            if($resultado_pessoa && $resultado_contato){
-                return "Apagado com sucesso";
-            }else{
-                return "erro ao apagar";
-            }
-        }
-
-        public function atualizar(Usuario $u){
-            $sql_usuario = "update pessoa set nome_pessoa=?, profissao_pessoa=? where cpf_pessoa =?";
-            $sql_contato = "update contato set telefone_contato=?, email_contato=? where pessoa_contato=?";
-
-            $bd = new Conexao();
-            $con = $bd->getConexao();
-
-            $valor_usuario = $con->prepare($sql_usuario);
-            $valor_usuario->bindValue(1, $u->getNome());
-            $valor_usuario->bindValue(2, $u->getProfissao());
-            $valor_usuario->bindValue(3, $u->getCpf());
-
-            $valor_contato = $con->prepare($sql_contato);
-            $valor_contato->bindValue(1, $u->getTelefone());
-            $valor_contato->bindValue(2, $u->getEmail());
-            $valor_contato->bindValue(3, $u->getCpf());      
-            
-            $resultado_pessoa = $valor_usuario->execute();
-            $resultado_contato = $valor_contato->execute();
-
-            if($resultado_pessoa && $resultado_contato){
-                return "Atualizado com sucesso";
-            }else{
-                return "erro ao atualizar";
-            }
-        }
-
 
         public function consultar(){
     
             // Método para consultar todas as pessoas e seus contatos
             // Realiza um join entre as tabelas 'pessoa' e 'contato'
-            $sql = "select * from pessoa inner join contato on pessoa.cpf_pessoa=contato.pessoa_contato";
+            $sql = "select * from TB_PRODUTO";
            
             // Obtém a conexão e executa a consulta
             $bd = new Conexao();
@@ -118,8 +28,103 @@ include "conexao.php";
                 // Retorna os resultados em um array associativo
                 $resultado = $valor->fetchAll(\PDO::FETCH_ASSOC);
                 return $resultado;
-            }else{
+            }
+            else{
                 return "erro";
+            }
+        }
+
+        public function consultarPorID(Produto $p){
+            $sql_produto = "select * from TB_PRODUTO where ID_PRODUTO=?";
+
+            $bd = new Conexao();
+            $con = $bd->getConexao();
+
+            $valor_produto = $con->prepare($sql_produto);
+            $valor_produto->bindValue(1, $p->getIDProduto());
+
+            $resultado_usuario = $valor_produto->execute();
+
+             // Verifica se foram encontrados registros
+            if($valor_produto->rowCount()>0){
+                // Retorna os resultados em um array associativo
+                $resultado = $valor_produto->fetchAll(\PDO::FETCH_ASSOC);
+                return $resultado;
+            }
+            else{
+                return "erro";
+            }
+        }
+
+        // Método para cadastrar um novo Cupom no banco de dados
+        // Insere dados na tabela 'Cupom'
+        public function cadastrar(Produto $p){
+
+            // SQL para inserir dados na tabela 'TB_PRODUTO'
+            $sql_produto = "insert into TB_PRODUTO (ID_PRODUTO, NOME_PRODUTO, PRECO_PRODUTO, DESCRICAO_PRODUTO, IMAGEM_PRODUTO) 
+            values (?, ?, ?, ?, ?)";
+
+            // $sql_categoria = "insert into TB_CATEGORIA (NOME_CATEGORIA) value (?)";
+
+            //instanciar o objeto de conexão
+            $bd = new Conexao();
+            $con = $bd->getConexao();
+
+            // Prepara e executa as consultas SQL
+            $valor_produto = $con->prepare($sql_produto);
+            $valor_produto->bindValue(1, $p->getIDProduto());
+            $valor_produto->bindValue(2, $p->getNomeProduto());
+            $valor_produto->bindValue(3, $p->getPrecoProduto()); 
+            $valor_produto->bindValue(4, $p->getDescricaoProduto()); 
+            $valor_produto->bindValue(5, $p->getImagemProduto());       
+
+            $resultado_produto = $valor_produto->execute();
+
+            // Verifica se ambas as inserções foram bem-sucedidas
+            if($resultado_produto){
+                return "cadastrado com sucesso";
+            }else{
+                return "erro ao cadastrar";
+            }
+        }
+
+        //os demais métodos seguem a mesma lógica do primeiro
+        public function deletar(Produto $p){
+            $sql_produto = "delete from TB_PRODUTO where ID_PRODUTO=?";
+
+            $bd = new Conexao();
+            $con = $bd->getConexao();
+
+            $valor_produto = $con->prepare($sql_produto);
+            $valor_produto->bindValue(1, $p->getIDProduto());
+            
+            $resultado_produto = $valor_produto->execute();
+
+            if($resultado_produto){
+                return "Apagado com sucesso";
+            }else{
+                return "erro ao apagar";
+            }
+        }
+
+        public function atualizar(Produto $p){
+            $sql_produto = "update TB_PRODUTO set NOME_PRODUTO=?, PRECO_PRODUTO=?, IMAGEM_PRODUTO=? DESCRICAO_PRODUTO=? where ID_PRODUTO =?";
+
+            $bd = new Conexao();
+            $con = $bd->getConexao();
+
+            $valor_produto = $con->prepare($sql_produto);
+            $valor_produto->bindValue(1, $p->getNomeProduto());
+            $valor_produto->bindValue(2, $p->getPrecoProduto());
+            $valor_produto->bindValue(3, $p->getImagemProduto());
+            $valor_produto->bindValue(4, $p->getDescricaoProduto());      
+            
+            $resultado_produto = $valor_produto->execute();
+
+            if($resultado_produto){
+                return "Atualizado com sucesso";
+            }else{
+                return "erro ao atualizar";
             }
         }
     }
