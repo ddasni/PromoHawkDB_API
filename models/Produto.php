101747f169ca6af60f5f738998ef70f248f95fc5
ddasni <danielsousamelo5@gmail.com>
2025-04-18 18:26:52 -0300

    Adicionando o id do produto

--- models/Produto.php
+++ models/Produto.php
@@ -4,40 +4,48 @@
 
   class Produto{
     // Atributos privados da classe Produto
-    private $nomeProduto, $preco, $descricao, $imagem;
+    private $idProduto, $nomeProduto, $preco, $descricao, $imagem;
 
-    // Método para definir o preco da produto
+    // Método para definir o id do produto
+    public function setIDProduto($idProduto){
+        $this->idProduto = $idProduto;
+    }
+
+    public function getIDProduto(){
+        return $this->idProduto;
+    }
+
+    // os outros métodos getters e setters para os demais atributos
     public function setNomeProduto($nomeProduto){
         $this->nomeProduto = $nomeProduto;
     }
 
-    public function getNome(){
+    public function getNomeProduto(){
         return $this->nomeProduto;
     }
 
-    // os outros métodos getters e setters para os demais atributos
-    public function setPreco($preco){
+    public function setPrecoProduto($preco){
         $this->preco = $preco;
     }
 
     // Método para obter o preco do produto
-    public function getPreco(){
+    public function getPrecoProduto(){
         return $this->preco;
     }
 
-    public function setDescricao($descricao){
+    public function setDescricaoProduto($descricao){
         $this->descricao = $descricao;
     }
 
-    public function getDescricao(){
+    public function getDescricaoProduto(){
         return $this->descricao;
     }
 
-    public function setImagem($imagem){
+    public function setImagemProduto($imagem){
         $this->imagem = $imagem;
     }
 
-    public function getImagem(){
+    public function getImagemProduto(){
         return $this->imagem;
     }
   }
